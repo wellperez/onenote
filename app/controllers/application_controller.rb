4b77298b55c73760fd8e53d67414e2775f61ba07
Wellington <wellingtonaperez@gmail.com>
2017-03-29 09:38:53 -0300

    Creation Of Controllers (Notes, Tags and Users) and set routes fo this controllers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,6 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :authenticate_user!
   include CanCan::ControllerAdditions
+
+  skip_before_filter :verify_authenticity_token
 end
